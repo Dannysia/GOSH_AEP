f55358641ad1f8fd37cca1bef95ad5178038cda2
N-Hagerdorn <62390366+N-Hagerdorn@users.noreply.github.com>
2021-11-06 17:25:48 -0400

    CSS boyos

--- common.js
+++ common.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
-    $("a").hover(menuButtonIn(this), menuButtonOut(this));
-    function(){
-      $(this).css("background-color", "var(--aep_light_red)");
-    }, 
-    function(){
-      $(this).css("background-color", "var(--aep_dark_red)");
-    }
+    $("a").hover(
+        function(){
+            $(this).css("background-color", "var(--aep_light_red)");
+            $(this).children('a').each(function() {$(this).css("color", "black");});
+            //for (var i = 0; i < $(this).children.length; i++) {
+               //$(this).children[i].css("visibility", "visible");
+                //$(this).children[i].css("color", "black");
+            //}
+        },
+        function(){
+            $(this).css("background-color", "var(--aep_dark_red)");
+            for (var i = 0; i < $(this).children.length; i++) {
+                $(this).children[i].css("visibility", "hidden");
+                $(this).children[i].css("color", "white");
+            }
+        }
     );
-  });
-
-function menuButtonIn() {
-    $(this).css("background-color", "var(--aep_light_red)");
-}
-
-function menuButtonOut() {
-
-}
+});
