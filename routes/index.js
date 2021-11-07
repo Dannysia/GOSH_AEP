178edfbc655e3ebb25dc702d639e8564633acfa8
Dannysia <dsiatkosky@gmail.com>
2021-11-07 03:55:46 -0500

    database working :)

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,25 @@ router.get('/image/:img', function(req, res, next){
   res.sendFile('/home/danny/express/public/images/'+req.params["img"]+'.JPG');
 });
 
+router.get('/getLocations/', function(req, res, next) {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'danny3',
+    password: 'password',
+    database: 'vision',
+    insecureAuth: true
+  })
+
+  connection.connect()
+
+  connection.query('SELECT Latitude, Longitude, ImageID FROM PoleImages;', function (err, rows, fields) {
+    if (err) throw err
+    res.send(rows)
+  })
+
+connection.end()
+});
+
+
 module.exports = router;
