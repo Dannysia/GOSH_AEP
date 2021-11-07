58675ac0cab55bc38cc5f5ec7b5c2683b4097b69
N-Hagerdorn <62390366+N-Hagerdorn@users.noreply.github.com>
2021-11-07 01:48:34 -0400

    Yeah

--- map.js
+++ map.js
@@ -7,22 +7,37 @@ function loadMapScenario() {
         credentials: 'ArF9X74BvF7_gQ8upWm40PIJnSZDAvjM26t-E8e0hUQkFjzwjyiI5lMhpf9Ziw_F '
     });
 
-    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), null);
-    map.entities.push(pushpin);
+    //var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), null);
+    //map.entities.push(pushpin);
 
-    testMap(map);
+    placePins(map);
 }
 
-function testMap(map) {
+function placePins(map) {
+
     //Load the Clustering module.
     Microsoft.Maps.loadModule("Microsoft.Maps.Clustering", function() {
 
-        //Generate 1,000 random pushpins in the map view.
-        var pins = Microsoft.Maps.TestDataGenerator.getPushpins(1000, map.getBounds());
+        // Receive pairs of coordinates (lat, long) from the SQL server
+        lats = [39.9054895, 0, 0, 0.1];
+        longs = [116.3976317, 0, 0.1, 0];
+        pins = new Array(lats.length);
+
+        // For each coordinate pair (lat, long): place a pin
+        for (let i = 0; i < pins.length; i++) {
+            // Create a new pin,
+            pins[i] = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(lats[i], longs[i]));
+
+            // Assign an event handler to the pin
+            Microsoft.Maps.Events.addHandler(pins[i], 'click', function() {
+                window.location.replace("imagePortal.html?pin=" + i);
+            });
+
+            //map.entities.push(pins[i]);
+        }
 
         //Create a ClusterLayer and add it to the map.
         var clusterLayer = new Microsoft.Maps.ClusterLayer(pins);
         map.layers.insert(clusterLayer);
     });
 }
-
